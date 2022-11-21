a257aead07ab1e0509529709675f126d16b1dafd
shajn786 <shajn786@gmail.com>
2022-11-21 12:43:47 +0530

    smmallest

--- src/Main.java
+++ src/Main.java
@@ -15,16 +15,32 @@ public class Main {
         result = (num1 > num2) ? (num1 > num3 ? num1 : num3) : (num2 > num3 ? num2 : num3);
         System.out.println(result);
     }
+
+    static void smallest()
+    {
+        int num1,num2,num3,result;
+        System.out.println("enter the three numbers");
+        num1=sc.nextInt();
+        num2=sc.nextInt();
+        num3=sc.nextInt();
+
+        result = (num1 < num2) ? (num1 < num3 ? num1 : num3) : (num2 < num3 ? num2 : num3);
+        System.out.println(result);
+    }
     public static void main(String[] args) {
 
         System.out.println("enter the operation\n" +
-                "1 : Largest of three");
+                "1 : Largest of three\n" +
+                "2 : Smmallest of three Numbers");
         opt =sc.nextInt();
 
         switch(opt)
         {
             case 1:
                 largest();
+
+            case 2:
+                smallest();
         }
 
     }
