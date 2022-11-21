ea37ec8612ffd76aa1cd6e5947153031a0a61d00
shajn786 <shajn786@gmail.com>
2022-11-21 12:52:44 +0530

    prime

--- src/Main.java
+++ src/Main.java
@@ -27,11 +27,34 @@ public class Main {
         result = (num1 < num2) ? (num1 < num3 ? num1 : num3) : (num2 < num3 ? num2 : num3);
         System.out.println(result);
     }
+
+    static void prime()
+    {
+        int num,count=0;
+        System.out.println("enter the number to check prime or not");
+        num =sc.nextInt();
+
+        for(int i =2 ;i<=num-1;i++)
+        {
+            if(num%i== 0)
+            {
+                count++;
+            }
+        }
+
+        if(count == 0)
+
+            System.out.println("prime");
+        else
+            System.out.println("not prime");
+
+    }
     public static void main(String[] args) {
 
         System.out.println("enter the operation\n" +
                 "1 : Largest of three\n" +
-                "2 : Smmallest of three Numbers");
+                "2 : Smallest of three Numbers\n" +
+                "3 : Prime of a number");
         opt =sc.nextInt();
 
         switch(opt)
@@ -41,6 +64,9 @@ public class Main {
 
             case 2:
                 smallest();
+
+            case 3:
+                prime();
         }
 
     }
