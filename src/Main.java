14884d33adefff780194d2f8b53db7e67a7d1c73
shajn786 <shajn786@gmail.com>
2022-11-21 12:33:22 +0530

    largest of three

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
+
+    static int opt;
+    static Scanner sc = new Scanner(System.in);
+    static void largest()
+    {
+        int num1,num2,num3,result;
+        System.out.println("enter the three numbers");
+        num1=sc.nextInt();
+        num2=sc.nextInt();
+        num3=sc.nextInt();
+
+        result = (num1 > num2) ? (num1 > num3 ? num1 : num3) : (num2 > num3 ? num2 : num3);
+        System.out.println(result);
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("enter the operation\n" +
+                "1 : Largest of three");
+        opt =sc.nextInt();
+
+        switch(opt)
+        {
+            case 1:
+                largest();
+        }
+
     }
 }
